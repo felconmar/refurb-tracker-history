4b02024e0aebecbd2ac7f54198de833023999498
felconmar <felixconde35@gmail.com>
2024-03-23 12:37:27 +0100

    First init

---
+++ rss_updater.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+import feedparser
+
+
+# https://refurb-tracker.com/feeds/es_in_ipad_iphone_ipod_homepod_imac_imacpro_macpro_macstudio_mini_macbookpro_macbookair_watch_atv_acc.xml
+
+
+url = "https://refurb-tracker.com/feeds/es_in_all.xml"
+feed = feedparser.parse(url)
+
+# for entry in feed.entries:
+#     print("Entry Id:", entry.id)
+#     print("Entry Title:", entry.title)
+#     print("Entry Date:", entry.updated)
+#     print("Entry Link:", entry.link)
+#     print("\n")
+
+# print(feed.entries)
+
+
+# Use pd.json_normalize to convert the JSON to a DataFrame
+df = pd.json_normalize(feed.entries)
+# Select the main columns
+df = df[["id", "title", "updated", "link"]]
+
+# Rename the columns for clarity
+df.columns = ["Id", "Title", "Date", "Link"]
+
+# Display the DataFrame
+# print(df)
+
+# Store dataframe in csv file
+df.to_csv("es/refurb_tracker_all.csv", index=False)
